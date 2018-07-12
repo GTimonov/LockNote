7c1a2f46eff0a5bc8b04d17fee24bab2b9bfb583
Gtim <goga-kharkov@yandex.ru>
2018-07-12 23:14:32 +0300

    Changes to be committed: 	new file:   .vscode/launch.json 	new file:   launch.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/createNote/CreateNote.jsx 	new file:   src/components/mainWindow/Main.jsx 	new file:   src/components/mainWindow/main.css 	modified:   src/index.css 	deleted:    src/logo.svg

---
+++ src/components/mainWindow/Main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './main.css';
+class Main extends Component {
+    state = {  }
+    render() {
+        return (
+          <div className="main">
+              
+          </div>  
+        );
+    }
+}
+
+export default Main;
