ad42bb3843806adb35cff6ff3ee587bce0e20404
George T <G.Timonov@newline.tech>
2019-09-16 13:06:37 +0300

    comit

--- client/index.js
+++ client/index.js
@@ -3,13 +3,37 @@ import ReactDOM from 'react-dom';
 import Main from './components/Main';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import * as reducers from './store/reducers';
 
-const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
 
+const playlist = (state=[], action)=>{
+    if (action.type === 'ADD_TRACK'){
+        return [
+            ...state, 
+            action.payload
+        ]
+    }
+}
+ 
+const store = createStore(playlist);
+store.subscribe(()=>{
+    console.log('subscribe', store.getState())
+    const list = document.querySelectorAll('.list')[0];
+    store.getState().forEach(track => {
+        const li = document.createElement('li');
+        li.textContent = track;
+        list.appendChild(li);
+    });
+}); 
+
+const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+addTrackBtn.addEventListener('click', ()=>{
+    const trackName = document.querySelectorAll('.trackInput')[0].value;
+    store.dispatch({type:'ADD_TRACK', payload:trackName });
+
+})
 ReactDOM.render(
     <Provider store={store}>
         <Main/>
     </Provider>
 , document.getElementById("root"));
+    
