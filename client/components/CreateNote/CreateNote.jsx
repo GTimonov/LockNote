9bd93229e6a9dc300c6c8feeb0f4b7ae9f83be03
GTimonov <goga-kharkov@yandex.ru>
2020-06-16 23:32:16 +0300

    fix Greate Note  to hook

--- client/components/CreateNote/CreateNote.jsx
+++ client/components/CreateNote/CreateNote.jsx
@@ -1,28 +1,23 @@
-import React, {Component, useCallback} from 'react'
+import React, { useCallback} from 'react'
 import './styles.scss'
-
 import {inputChange} from '../../actions/creators'
-import {connect} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 const MAX_CHARS = 2000
 function CreateNote(props) {
+    const dispatch = useDispatch()
     const onChange = useCallback((e) => {
         const value = e.target.value
         //this.setState({value:value})
-        this.props.dispatch(inputChange(value))
-    }, [])
+        dispatch(inputChange(value))
+    }, [dispatch])
 
-    const {value} = this.props
-    const charsCount = value.length
+    const value = useSelector(state => state.inputText)
+    const charsCount = value ? value.length : 0
     return (
         <div className="create-note">
             <label htmlFor="" className="max-chars-lbl">{`${MAX_CHARS - charsCount}/${MAX_CHARS}`}</label>
-            <textarea type="text" onChange={onChange(e)} maxLength={MAX_CHARS} value={value} />
+            <textarea type="text" onChange={onChange} maxLength={MAX_CHARS} value={value} />
         </div>
     )
 }
-
-const mapStateToProps = (state, ownProps) => {
-    return {value: state.inputText}
-}
-
-export default connect(mapStateToProps)(CreateNote)
+export default CreateNote
